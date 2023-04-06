eea2d1a5d09ef8a69729c5810c91c2608c1d3856
Ric Cox <ric.cox@gmail.com>
2023-04-05 17:23:10 -0700

    name change

--- echo/main.go
+++ echo/main.go
@@ -1,75 +1,64 @@
-/*
- *  echo.go
- *
- *  A simple echo server
- *
- */
-
+ // A simple echo server
 package main
 
 import (
-  "bufio"
-  "flag"
-  "fmt"
-  "net"
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
 )
 
-
 func echo(client net.Conn) {
-  b := bufio.NewReader(client)
+	b := bufio.NewReader(client)
 
-  for {
-    line, err := b.ReadBytes('\n')  // read it
-     if err != nil {
-       break
-     }
+	for {
+		line, err := b.ReadBytes('\n') // read it
+		if err != nil {
+			break
+		}
 
-    client.Write(line)              // send it
-  }
+		client.Write(line) // send it
+	}
 }
-
 
 func Server(listener net.Listener) chan net.Conn {
-  channel := make(chan net.Conn)
-  i := 0
+	channel := make(chan net.Conn)
+	i := 0
 
-  go func() {
-    for {
-      client, err := listener.Accept()
-      if client == nil {
-        fmt.Printf("Accept() fail : ", err)
-        continue
-      }
+	go func() {
+		for {
+			client, err := listener.Accept()
+			if client == nil {
+				fmt.Printf("Accept() fail : ", err)
+				continue
+			}
 
-      i++
-      fmt.Printf("%d: %v <-> %v\n", i, client.LocalAddr(), client.RemoteAddr())
-      channel <- client
-   }
-  }()
+			i++
+			fmt.Printf("%d: %v <-> %v\n", i, client.LocalAddr(), client.RemoteAddr())
+			channel <- client
+		}
+	}()
 
-  return channel
+	return channel
 }
-
 
 var port, help string
 
 func init() {
-  flag.StringVar(&port, "port",  "2345", "Server bind port")
+	flag.StringVar(&port, "port", "2345", "Server bind port")
 }
-
 
 func main() {
-  flag.Parse()
+	flag.Parse()
 
-    server, err := net.Listen("tcp", ":" + port)
-    if server == nil {
-        panic(err)
-    }
+	server, err := net.Listen("tcp", ":"+port)
+	if server == nil {
+		panic(err)
+	}
 
-    client := Server(server)
+	client := Server(server)
 
-    for {
-        go echo(<-client)
-    }
+	for {
+		go echo(<-client)
+	}
 }
-
--- relay/main.go
+++ relay/main.go
@@ -1,97 +1,93 @@
-/*
- *  relay.go
- *
- *  A simple relay server
- */
-
+// A messaging relay
 package main
 
 import (
-  "bufio"
-  "flag"
-  "fmt"
-  "net"
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
+	"strconv"
 )
 
-
-var dstip, dstport, port string
-
-func init() {
-  flag.StringVar(&dstport, "dstip",   "127.0.0.1", "Relay destination IP")
-  flag.StringVar(&dstport, "dstport", "3456",      "Relay destination port")
-  flag.StringVar(&port,    "port",    "2345",      "Server bind port")
-}
-
-
 func relay(message []byte) string {
-  status := "failed"
-  c, err := net.Dial("tcp", dstip + ":" + dstport)
-  if err != nil {
-    return status
-  }
+	status := "failed"
+	c, err := net.Dial("tcp", dstip+":"+dstport)
+	if err != nil {
+		return status
+	}
 
-  c.Write(message)
-  if err != nil {
-    return status
-  }
+	c.Write(message)
+	if err != nil {
+		return status
+	}
 
-  status = "success"
-  c.Close()
-  return status
+	status = "success"
+	c.Close()
+	return status
 }
-
 
 func handler(client net.Conn) {
-  b := bufio.NewReader(client)
+	b := bufio.NewReader(client)
 
-  for {
-    line, err := b.ReadBytes('\n')
-     if err != nil {
-       break
-     }
+	for {
+		line, err := b.ReadBytes('\n')
+		if err != nil {
+			break
+		}
 
-    status := relay(line);
-    b := []byte("relay " + status + "\n")
-    client.Write(b)
-  }
+		status := relay(line)
+		b := []byte("relay " + status + "\n")
+		client.Write(b)
+	}
 }
-
 
 func Server(listener net.Listener) chan net.Conn {
-  channel := make(chan net.Conn)
-  i := 0
+	channel := make(chan net.Conn)
+	i := 0
 
-  go func() {
-    for {
-      client, err := listener.Accept()
-      if client == nil {
-        fmt.Printf("Accept() fail : ", err)
-        continue
-      }
+	go func() {
+		for {
+			client, err := listener.Accept()
+			if client == nil {
+				fmt.Printf("Accept() fail : ", err)
+				continue
+			}
 
-      i++
-      fmt.Printf("%d: %v <-> %v\n", i, client.LocalAddr(), client.RemoteAddr())
-      channel <- client
-   }
-  }()
+			i++
+			fmt.Printf("%d: %v <-> %v\n", i, client.LocalAddr(), client.RemoteAddr())
+			channel <- client
+		}
+	}()
 
-  return channel
+	return channel
 }
-
 
 func main() {
-  flag.Parse()
+	var (
+		serverDst = flag.String("dst", "127.0.0.1", "Destination IP or name")
+		portDst   = flag.Int("dport", 5000, "Destination port")
+		serverSrc = flag.String("src", 8000, "Souce IP or name")
+		portSrc   = flag.Int("sport", 5000, "Source port")
+	)
+	flag.Parse()
 
-  server, err := net.Listen("tcp", ":" + port)
-  if server == nil {
-    panic(err)
-  }
+	rx := *serverSrc + ":" + strconv.Itoa(*portSrc)
+	tx := *serverDst + ":" + strconv.Itoa(*portDst)
 
-  client := Server(server)
+	dst := DestinationConnect(tx)
+	defer dst.Close()
 
-  for {
-    go handler(<-client)
-  }
+	listener := SourceConnect(rx)
+	defer listener.Close()
+
+	server, err := net.Listen("tcp", ":"+port)
+	if server == nil {
+		panic(err)
+	}
+
+	client := Server(server)
+
+	for {
+		go handler(<-client)
+	}
 }
-
-
